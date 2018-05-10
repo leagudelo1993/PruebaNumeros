212af2341046cea4551a349838f6399fb1cb8d6f
León Agudelo <leagudelo@personalsoft.com.co>
2018-05-10 10:19:35 -0500

    Se agrega detalle a los mensajes del ejercicio

--- Solution/Frecuencia/Program.cs
+++ Solution/Frecuencia/Program.cs
@@ -26,7 +26,7 @@ namespace Frecuencia
                 resultValue = 0;
             }
             else
-                Close("El tamaño debe ser númerico");
+                Close("El valor debe ser númerico, presione ENTER para salir");
 
 
 
@@ -39,19 +39,19 @@ namespace Frecuencia
                 resultValue = 0;
             }
             else
-                Close("El tamaño debe ser númerico");
+                Close("El valor debe ser númerico, presione ENTER para salir");
 
             // Constraint: n<=m
             if (m <= n)
-                Close("El tamaño del segundo arreglo debe ser mayor al del primer arreglo");
+                Close("El tamaño del segundo arreglo debe ser mayor al del primer arreglo, presione ENTER para salir");
 
 
             // Constraint: 1<=n,m<=2*100000
             if ((n < 1 || n >= 200000) || (m < 1 || m >= 200000))
-                Close("El tamaño de los arreglos deben estar entre 1 y 200000");
+                Close("El tamaño de los arreglos deben estar entre 1 y 200000, presione ENTER para salir");
 
 
-            Console.WriteLine("Escribe los valores del primer arreglo");
+            Console.WriteLine("Escribe los valores del primer arreglo, línea por línea");
             arr = new int[n];
             for (int i = 0; i < n; i++)
             {
@@ -61,12 +61,12 @@ namespace Frecuencia
                 if (resultValue != 0)
                     arr[i] = resultValue;
                 else
-                    Close("Los valores deben ser numéricos");
+                    Close("Los valores deben ser numéricos, presione ENTER para salir");
 
                 resultValue = 0;
             }
 
-            Console.WriteLine("Escribe los valores del segundo arreglo");
+            Console.WriteLine("Escribe los valores del segundo arreglo, línea por línea");
             brr = new int[m];
             for (int i = 0; i < m; i++)
             {
@@ -76,21 +76,21 @@ namespace Frecuencia
                 if (resultValue != 0)
                     brr[i] = resultValue;
                 else
-                    Close("Los valores deben ser numéricos");
+                    Close("Los valores deben ser numéricos, presione ENTER para salir");
 
                 resultValue = 0;
             }
 
             // Constraint: 1<=x <=10000
             if ((arr.Where(k => k <= 1 || k >= 10000).Count() > 0) || (brr.Where(k => k <= 1 || k >= 10000).Count() > 0))
-                Close("Los valores de los arreglos deben estar entre 1 y 10000");
+                Close("Los valores de los arreglos deben estar entre 1 y 10000, presione ENTER para salir");
 
 
             int[] result = NumericalOrdering.missingNumbers(arr, brr, out diff);
 
             // Constraint: Xmax - Xmin < 101
             if (diff > 100)
-                Close("La diferencia entre el valor máximo y minimo del segundo arreglo no debe ser menor o igual a 100");
+                Close("La diferencia entre el valor máximo y minimo del segundo arreglo no debe ser menor o igual a 100, presione ENTER para salir");
 
 
             Close(string.Concat("Los números faltantes son: ", String.Join(" ", result)));
